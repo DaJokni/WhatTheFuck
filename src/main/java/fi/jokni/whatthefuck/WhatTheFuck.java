81dbdc7813518965bc44afd522c513a44936e6a8
DaJokni <56400974+DaJokni@users.noreply.github.com>
2022-04-24 16:03:07 +0300

    sheesh

---
+++ src/main/java/fi/jokni/whatthefuck/WhatTheFuck.java
@@ -0,0 +1,24 @@
+package fi.jokni.whatthefuck;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WhatTheFuck extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this,this);
+
+    }
+
+    @EventHandler
+    public void onjoin(PlayerJoinEvent e){
+        Player p =e.getPlayer();
+        if (p.isDead()){
+            p.spigot().respawn();
+        }
+    }
+}
